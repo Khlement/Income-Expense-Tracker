7b166705ebb64225d4f1fe71528bfc66bad70610
georgemurage00 <71360279+georgemurage00@users.noreply.github.com>
2023-06-20 14:55:10 +0900

    added new files

---
+++ app2.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 20 13:26:23 2023
+
+@author: khlement
+"""
+
+import calendar
+from datetime import datetime
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+import plotly.graph_objects as go
+import pymongo
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+mongodb_password = os.getenv("MONGODB_PASSWORD")
+
+# -------------- SETTINGS --------------
+incomes = ["Salary", "Blog", "Other Income"]
+expenses = ["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
+currency = "USD"
+page_title = "Income and Expense Tracker"
+page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+layout = "centered"
+# --------------------------------------
+
+# MongoDB Atlas connection setup
+connection_string = f"mongodb+srv://khlement:{mongodb_password}@cluster1.rioqscn.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(connection_string)
+db = client["IncomeExpenseData"]
+collection = db["IncomeData"]
+
+
+# --- DROP DOWN VALUES FOR SELECTING THE PERIOD ---
+years = [datetime.today().year, datetime.today().year + 1]
+months = list(calendar.month_name[1:])
+
+
+# --- DATABASE INTERFACE ---
+def get_all_periods():
+    periods = [item["key"] for item in collection.find()]
+    return periods
+
+
+# --- HIDE STREAMLIT STYLE ---
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# --- NAVIGATION MENU ---
+selected = option_menu(
+    menu_title=None,
+    options=["Data Entry", "Data Visualization"],
+    icons=["pencil-fill", "bar-chart-fill"],  # https://icons.getbootstrap.com/
+    orientation="horizontal",
+)
+
+
+# --- INPUT & SAVE PERIODS ---
+if selected == "Data Entry":
+    st.header(f"Data Entry in {currency}")
+    with st.form("entry_form", clear_on_submit=True):
+        col1, col2 = st.columns(2)
+        col1.selectbox("Select Month:", months, key="month")
+        col2.selectbox("Select Year:", years, key="year")
+
+        "---"
+        with st.expander("Income"):
+            for income in incomes:
+                st.number_input(f"{income}:", min_value=0, format="%i", step=10, key=income)
+        with st.expander("Expenses"):
+            for expense in expenses:
+                st.number_input(f"{expense}:", min_value=0, format="%i", step=10, key=expense)
+        with st.expander("Comment"):
+            comment = st.text_area("", placeholder="Enter a comment here ...")
+
+        "---"
+        submitted = st.form_submit_button("Save Data")
+        if submitted:
+            period = f"{st.session_state['year']}_{st.session_state['month']}"
+            incomes = {income: st.session_state[income] for income in incomes}
+            expenses = {expense: st.session_state[expense] for expense in expenses}
+            report = {
+                "key": period,
+                "incomes": incomes,
+                "expenses": expenses,
+                "comment": comment,
+            }
+            collection.insert_one(report)
+            st.success("Data saved!")
+
+
+# --- PLOT PERIODS ---
+if selected == "Data Visualization":
+    st.header("Data Visualization")
+    with st.form("saved_periods"):
+        period = st.selectbox("Select Period:", get_all_periods())
+        submitted = st.form_submit_button("Plot Period")
+        if submitted:
+            # Get data from database
+            report = collection.find_one({"key": period})
+            comment = report.get("comment")
+            expenses = report.get("expenses")
+            incomes = report.get("incomes")
+
+            # Create metrics
+            total_income = sum(incomes.values())
+            total_expense = sum(expenses.values())
+            remaining_budget = total_income - total_expense
+            col1, col2, col3 = st.columns(3)
+            col1.metric("Total Income", f"{total_income} {currency}")
+            col2.metric("Total Expense", f"{total_expense} {currency}")
+            col3.metric("Remaining Budget", f"{remaining_budget} {currency}")
+            st.text(f"Comment: {comment}")
+
+            # Create sankey chart
+            label = list(incomes.keys()) + ["Total Income"] + list(expenses.keys())
+            source = list(range(len(incomes))) + [len(incomes)] * len(expenses)
+            target = [len(incomes)] * len(incomes) + [label.index(expense) for expense in expenses.keys()]
+            value = list(incomes.values()) + list(expenses.values())
+
+            # Data to dict, dict to sankey
+            link = dict(source=source, target=target, value=value)
+            node = dict(label=label, pad=20, thickness=30, color="#E694FF")
+            data = go.Sankey(link=link, node=node)
+
+            # Plot it!
+            fig = go.Figure(data)
+            fig.update_layout(margin=dict(l=0, r=0, t=5, b=5))
+            st.plotly_chart(fig, use_container_width=True)
